7197709542829dfc3ac2f4007d9907e0caa55250
Ashar <asharmehmood99@gmail.com>
2025-07-09 02:19:41 +0500

    4th task

---
+++ 1- Two Sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        map = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in map:
+                return map[diff], i
+            map[num] = i
