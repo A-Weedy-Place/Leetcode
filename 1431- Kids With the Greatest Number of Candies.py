75049008e1e091de642cc21c635e9e4c5348300a
Ashar <asharmehmood99@gmail.com>
2025-07-14 16:03:17 +0500

    1431

---
+++ 1431- Kids With the Greatest Number of Candies.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        a = max(candies)
+        for x in range(len(candies)):
+            candies[x] += extraCandies
+            if candies[x] >= a:
+                candies[x] = True
+            else:
+                candies[x] = False
+        return candies
