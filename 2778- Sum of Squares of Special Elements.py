1a7ef52a4e41d644c1f1e42a415a1e91c05506ee
Ashar <asharmehmood99@gmail.com>
2025-07-16 16:10:50 +0500

    2778

---
+++ 2778- Sum of Squares of Special Elements.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def sumOfSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        a = 0
+        for x in range(1, n+1):
+            if n % x == 0:
+                a = a + (nums[x-1]**2)
+        return a
