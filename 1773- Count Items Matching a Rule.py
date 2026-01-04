ea3ce78bed349e00eb9a445f3cabde61dae0c4f4
Ashar <asharmehmood99@gmail.com>
2026-01-04 15:53:21 +0500

    1773

---
+++ 1773- Count Items Matching a Rule.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def countMatches(self, items, ruleKey, ruleValue):
+        """
+        :type items: List[List[str]]
+        :type ruleKey: str
+        :type ruleValue: str
+        :rtype: int
+        """
+        x=0
+        for i in items:
+            if ruleKey == "type":
+                if i[0] == ruleValue:
+                    x+=1
+            elif ruleKey == "color":
+                if i[1] == ruleValue:
+                    x+=1
+            elif ruleKey == "name":
+                if i[2] == ruleValue:
+                    x+=1
+        return x
