58aa96f4c3e2541db1703937a3da2749314b77c6
Ashar <asharmehmood99@gmail.com>
2026-01-12 23:38:09 +0500

    2706

---
+++ 2706- Buy two Chocolates.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def buyChoco(self, prices, money):
+        """
+        :type prices: List[int]
+        :type money: int
+        :rtype: int
+        """
+        prices = sorted(prices)
+        if (prices[0]+prices[1] <= money):
+            return money - (prices[0]+prices[1])
+        else:
+            return money
