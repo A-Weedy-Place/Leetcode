9bc95d6f38ab04f599a375bf3bd746bf4bc5276d
Ashar <asharmehmood99@gmail.com>
2026-04-21 22:29:56 +0500

    1266

---
+++ 1266- Minimum Time Visiting All Points.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        sec = 0
+        
+        for i in range(1, len(points)):
+            x1, y1 = points[i-1]
+            x2, y2 = points[i]
+            
+            sec += max(abs(x2 - x1), abs(y2 - y1))
+        
+        return sec
