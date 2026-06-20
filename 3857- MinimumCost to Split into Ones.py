0143e83d96421b7a4285b00aad7ab3486addc5ca
Ashar <asharmehmood99@gmail.com>
2026-06-20 17:04:56 +0500

    3875

---
+++ 3857- MinimumCost to Split into Ones.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minCost(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n * (n - 1) // 2
+
+        # I was trying to do a recursive factorial logic but coulnt get it to work
+
+        # I cheated and i am fucking ashamed.
+        # I am a bitch and a piece of fking shit.
+
+        # Even though ik i cheated i am still submitting it bcz i have to continue my streak which pushes me to keep doing it everyday.
