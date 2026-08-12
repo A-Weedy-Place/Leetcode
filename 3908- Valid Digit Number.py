cd76bf2ddff42f0eaba5b651533448ddc7a7340e
Ashar <asharmehmood99@gmail.com>
2026-08-12 22:08:52 +0500

    3908

---
+++ 3908- Valid Digit Number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def validDigit(self, n, x):
+        """
+        :type n: int
+        :type x: int
+        :rtype: bool
+        """
+        n = str(n)
+        if n[0] == str(x):
+            return False
+        for i in n:
+            if i == str(x):
+                return True
+        return False
