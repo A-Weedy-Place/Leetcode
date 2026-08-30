ccf6f4ca87387aee3b6cd5ba55ec9f734c3e05d5
Ashar <asharmehmood99@gmail.com>
2026-08-30 21:08:28 +0500

    1769

---
+++ 1769- Minimum Number of Operations to Move All Balls to Each Box.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minOperations(self, boxes):
+        """
+        :type boxes: str
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(len(boxes)):
+            sum = 0
+            for j in range(len(boxes)):
+                if boxes[j] == "1":
+                    sum += abs(i-j)
+            ans.append(sum)
+        
+        return ans
